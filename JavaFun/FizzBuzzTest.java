b253e5b66873351dfcffac1472320b3e1047ef9b
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-09-30 14:47:02 -0500

    Sept 30 2019

---
+++ JavaFun/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest{
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.fizzBuzz(17);
+        System.out.println(result);
+
+    }
+}
---
+++ JavaFun/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+    public static void main(String[] args) {
+        Pythagorean num= new Pythagorean();
+        double result = num.calculateHypotenuse(5, 6);
+        System.out.println(result);
+    }
+}
