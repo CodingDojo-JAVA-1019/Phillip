9868ea7479ccbad128c71f0aed6f577c73b576de
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-09-30 14:53:38 -0500

    Delete PythagoreanTest.java

--- PythagoreanTest.java
+++
@@ -1,7 +0,0 @@
-public class PythagoreanTest{
-    public static void main(String[] args) {
-        Pythagorean num= new Pythagorean();
-        double result = num.calculateHypotenuse(5, 6);
-        System.out.println(result);
-    }
-}
